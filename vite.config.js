e91efa65757279e29e1eb3d2cb8fe36d17c678ed
PBhatia84 <pbhatia@elevatepsychiatry.com>
2026-03-04 19:17:41 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Replace 'harvest-ai' below with your exact GitHub repository name
 export default defineConfig({
   plugins: [react()],
-  base: '/harvest-ai/',
+  base: '/Harvest-AI/',
 })
